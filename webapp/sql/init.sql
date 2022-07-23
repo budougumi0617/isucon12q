6dd22ce39d30a32f1e2f866ea25254baf496b6be
yopidax <y.y.yopidax07@gmail.com>
2022-07-23 13:46:32 +0900

    feat: 書くところが間違っていた

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -2,3 +2,4 @@ DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
+create index competition_id_idx on visit_history(competition_id, player_id);
--- webapp/sql/tenant/11_create_index_visit_history_competion_id_idx.sql
+++
@@ -1,1 +0,0 @@
-create index competition_id_idx on visit_history(competition_id, player_id);
